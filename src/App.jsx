1cf47ae75bd89001cf091ad4c9369e840165c214
aayman01 <ayman.alfi098@gmail.com>
2024-03-15 16:12:45 +0600

    fetching data blog.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Header from './Components/Header/Header'
 
 function App() {
   
   return (
     <>
-      
-      <h1 className='text-6xl bg-red-600'>Knowladge Cafe</h1>
-      
+      <Header></Header>
+      <Blogs></Blogs>
       
     </>
   )
